164fee8f639ee0305ee016f0547142f3c72e1f2b
Hellorld! <jeremy.thornton@nhs.net>
2026-03-22 16:16:44 +0000

    tests

---
+++ src/TEST/test_dos.h
@@ -0,0 +1,9 @@
+#include "test_dos_files.h"
+#include "test_dos_memory.h"
+#include "test_dos_services.h"
+
+void test_dos() {
+    test_dos_files();
+    test_dos_memory();
+    test_dos_services();
+}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+#include "TEST/test_dos.h"
 
 int main() {
     printf("DOSLIB tests\n");
-
+    test_dos();
     return 0;
 }
